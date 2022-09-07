09192603e6231cfd9ebe73b75d289be0c0a85758
Jonatas Santos Da Silva <66139215+Jonattaz@users.noreply.github.com>
2022-09-07 20:13:20 -0300

    - Character Movement 2

--- Assets/Scripts/Comp_CharacterController.cs
+++ Assets/Scripts/Comp_CharacterController.cs
@@ -7,8 +7,23 @@ namespace PudimdimGames
 
     public class Comp_CharacterController : MonoBehaviour
     {
+        [Header("Movement")]
+        [SerializeField] private float _runSpeed = 6f;
+        [SerializeField] private float _sprintSpeed = 8f;
+
+        [Header("Sharpness")]
+        [SerializeField] private float _rotationSharpness = 10f;
+        [SerializeField] private float _moveSharpness = 10f;
+
         private Animator _animator;
         private Comp_PlayerInputs _inputs;
+
+        private float _targetSpeed;
+        private Quaternion _targetRotation;
+
+        private float _newSpeed;
+        private Vector3 _newVelocity;
+        private Quaternion _newRotation;
 
         // Start is called before the first frame update
         void Start()
@@ -22,8 +37,42 @@ namespace PudimdimGames
         // Update is called once per frame
         void Update()
         {
-        
+            // Input Handle
+            Vector3 _moveInputVector = new Vector3(_inputs.MoveAxisRightRaw, 0, _inputs.MoveAxisForwardRaw).normalized;
+            
+            // Move Speed
+            if(_inputs.Sprint.Pressed()){  _targetSpeed = _moveInputVector != Vector3.zero ? _sprintSpeed : 0; }
+            else                        {  _targetSpeed = _moveInputVector != Vector3.zero ? _runSpeed : 0; }
+            _newSpeed = Mathf.Lerp(_newSpeed, _targetSpeed, Time.deltaTime * _moveSharpness);
+            
+            // Velocity
+            _newVelocity = _moveInputVector * _newSpeed;
+            transform.Translate(_newVelocity * Time.deltaTime, Space.World);
+
+            // Rotation
+            if(_targetSpeed != 0){
+                _targetRotation = Quaternion.LookRotation(_moveInputVector);
+                _newRotation = Quaternion.Slerp(transform.rotation, _targetRotation, Time.deltaTime * _rotationSharpness);
+                transform.rotation = _newRotation;
+            }
+
+            
+
+            // Animations
+            _animator.SetFloat("Forward", _newSpeed);
+            
         }
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
--- Assets/Scripts/Comp_PlayerInputs.cs
+++ Assets/Scripts/Comp_PlayerInputs.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 namespace PudimdimGames
 {
     [System.Serializable]
-    public class Input
-    {   
+    public class Input{   
         public KeyCode primary;
         public KeyCode alternate;
 
@@ -24,15 +23,38 @@ namespace PudimdimGames
 
     }
 
-    public class Comp_PlayerInputs : MonoBehaviour
-    {
+    public class Comp_PlayerInputs : MonoBehaviour{
         public Input Forward;
         public Input Backward;
         public Input Right;
         public Input Left;
         public Input Sprint;
 
-        
+        public int MoveAxisForwardRaw{
+            get{
+                if(Forward.Pressed() && Backward.Pressed()){return 0;}
+                else if(Forward.Pressed()){return 1;}
+                else if(Backward.Pressed()){return -1;}
+                else{return 0;}
+            }
+        }
+
+        public int MoveAxisRightRaw{
+            get{
+                if(Right.Pressed() && Left.Pressed()){ return 0;}
+                else if(Right.Pressed()){return 1;}
+                else if(Left.Pressed()){return -1;}
+                else{return 0;}
+            }
+        }
+
+        public const string MouseXString = "Mouse X";
+        public const string MouseYString = "Mouse Y";
+        public const string MouseScrollString = "Mouse ScrollWheel";
+
+        public static float MouseXInput{get => UnityEngine.Input.GetAxis(MouseXString);}
+        public static float MouseYInput{get => UnityEngine.Input.GetAxis(MouseYString);}
+        public static float MouseScrollInput{ get => UnityEngine.Input.GetAxis(MouseScrollString);}
 
     }
 }
