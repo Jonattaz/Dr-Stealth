97ac817f8250b045ac9d240b58d76f835b09d5fb
Jonattaz <jonatas.silva159@etec.sp.gov.br>
2022-11-24 15:17:24 -0300

    - Font. Asset e fixes

--- Assets/Scripts/Environment/Teleporting.cs
+++ Assets/Scripts/Environment/Teleporting.cs
@@ -7,6 +7,7 @@ namespace PudimdimGames{
     
     public class Teleporting : MonoBehaviour
     {
+        [SerializeField] private bool outOfBound;
         [SerializeField] private Transform teleportTarget;
         [SerializeField] private GameObject player;
         public bool canTeleport;
@@ -14,16 +15,24 @@ namespace PudimdimGames{
         [SerializeField] private bool endGame;
         [SerializeField] private bool hasQTE;  
         [SerializeField] private string EndGameScene;
+        
+        
         void OnTriggerEnter(Collider other){
-             
-            if(!canTeleport && hasQTE){
-                QTE.SetActive(true);
-                CountDownTimer.TimerInstance.canCount = true;
-            }else{
-                QTE.SetActive(false);
-                CountDownTimer.TimerInstance.canCount = false;
-                CountDownTimer.TimerInstance.restart = true;
-                CountDownTimer.TimerInstance.noise = false;
+             if(outOfBound){
+                CameraFade.FadeInstance.Fade();
+                StartCoroutine(Teleport());
+            }
+
+            if(!outOfBound){    
+                if(!canTeleport && hasQTE){
+                    QTE.SetActive(true);
+                    CountDownTimer.TimerInstance.canCount = true;
+                }else{
+                    QTE.SetActive(false);
+                    CountDownTimer.TimerInstance.canCount = false;
+                    CountDownTimer.TimerInstance.restart = true;
+                    CountDownTimer.TimerInstance.noise = false;
+                }
             }
         }
 
@@ -37,25 +46,26 @@ namespace PudimdimGames{
                     StartCoroutine(Teleport());
                     CameraFade.FadeInstance.Fade();
                     player.GetComponent<Comp_CharacterController>().enabled = false;
-               } 
-                
+               }                
             }
-
         }
-    
          /// <summary>
          /// OnTriggerExit is called when the Collider other has stopped touching the trigger.
          /// </summary>
          /// <param name="other">The other Collider involved in this collision.</param>
          void OnTriggerExit(Collider other){
-            QTE.SetActive(false);
-            CountDownTimer.TimerInstance.canCount = false;
-            CountDownTimer.TimerInstance.restart = true;
-            CountDownTimer.TimerInstance.noise = false;
+           if(!outOfBound){
+                QTE.SetActive(false);
+                CountDownTimer.TimerInstance.canCount = false;
+                CountDownTimer.TimerInstance.restart = true;
+                CountDownTimer.TimerInstance.noise = false;
+           }
          }
 
         IEnumerator Teleport(){
             yield return new WaitForSeconds(1);
+             if(outOfBound)
+                CameraFade.FadeInstance.Fade();
             player.transform.position = new Vector3(
                 teleportTarget.transform.position.x,
                 teleportTarget.transform.position.y,
